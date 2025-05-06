2d227c4d7ee0301f33f5380a95eafe45f619f233
Bruno Moreira <moreira.bruno10@gmail.com>
2025-05-06 00:51:45 -0300

    first commit

--- src/components/categories/styles.ts
+++ src/components/categories/styles.ts
@@ -5,7 +5,7 @@ export const s = StyleSheet.create({
         maxHeight: 36,
         position: "absolute",
         zIndex: 1,
-        top: 48,
+        top: 46,
     },
     content: {
         gap: 8,
