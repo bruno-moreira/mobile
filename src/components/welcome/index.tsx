06f91471d336b5ad0a9df8aa2ebd533ba326f8f2
Bruno Moreira <moreira.bruno10@gmail.com>
2025-05-06 23:12:57 -0300

    update de ordem das categorias e nomeclatura

--- src/components/welcome/index.tsx
+++ src/components/welcome/index.tsx
@@ -7,7 +7,7 @@ export function Welcome(){
         <View>
             <Image source={require("@/assets/logo.png")} style={s.logo} />
 
-            <Text style={s.title}>Boas vindas ao Nearby!</Text>
+            <Text style={s.title}>Boas vindas ao Mobilidade!</Text>
             <Text style={s.subtitle}>Tenha cupons de vantagem para usar em {"\n"}
                 seus estabelecimentos favoritos
             </Text>
--- src/utils/categories-icons.ts
+++ src/utils/categories-icons.ts
@@ -8,7 +8,7 @@ import {
 } from "@tabler/icons-react-native"
 
 export const categoriesIcons: Record<string, React.ComponentType<IconProps>> = {
-  "abce52cf-b33b-4b3c-8972-eb72c66c83e4": IconHealthRecognition,
+  "12ce52cf-b33b-4b3c-8972-eb72c66c83e4": IconHealthRecognition,
   "146b1a88-b3d3-4232-8b8f-c1f006f1e86d": IconToolsKitchen2,
   "52e81585-f71a-44cd-8bd0-49771e45da44": IconCarGarage,
   "57d6e5ff-35f6-4d21-a521-84f23d511d25": IconHomeMove,
